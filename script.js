c0c32eb6f7b7f3a9a21b832a2cae446528656284
Hector V <a2193040328@alumnos.uat.edu.mx>
2024-09-05 21:40:07 -0600

    agregando fichas de ejercicios

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const portafolioDrop = document.getElementById('Portafolio');
 const navlink = document.getElementById('contenido-drop');
 
 portafolioDrop.addEventListener('click', (event) => {
-    event.preventDefault();
-    navlink.classList.toggle('active');
+    if (!navlink.classList.contains('active')) {
+        event.preventDefault(); // Prevenir la redirección si se muestra el menú
+        navlink.classList.add('active'); // Mostrar el dropdown
+    } else {
+        // Si está activo y se hace clic, redirige
+        window.location.href = 'portafolio.html';
+    }
 });
+
+// document.addEventListener('click', (event) => {
+//     if (!portafolioDrop.contains(event.target) && !navlink.contains(event.target)) {
+//         navlink.classList.remove('active'); // Cerrar el dropdown
+//     }
+// });
